7ada2ffdc8e027ae70711eb2dbae3568f2a5c0d6
123wwwa <123wwa@naver.com>
2022-05-14 00:05:38 +0900

    Update cancelLogout.js

--- save-time-test/pageScript/cancelLogout.js
+++ save-time-test/pageScript/cancelLogout.js
@@ -1,10 +1,13 @@
 (function() {
     var proxied = window.bbSessionActivityTimer.showDialog;
+
     window.bbSessionActivityTimer.showDialog = function() {
-        this.checkTimeRemainingBeforeTimeout();
+        //this.checkTimeRemainingBeforeTimeout();
+        this.keepBbSessionActive();
         console.log("Canceled Logout");
         return proxied.apply(this, [].slice.call(arguments));
     }
+    window.bb_dialogs.bb_confirm = function() { return; } // cancel LogOut Popup
     window.bbSessionActivityTimer.keepBbSessionActiveIfNotTimedOut = function() {
         this.keepBbSessionActive();
         console.log("Canceled Logout");

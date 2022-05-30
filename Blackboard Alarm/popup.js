25364abad438408f2997dfef751d7969bba8f731
SHA-01 <123wwa@naver.com>
2022-05-31 03:14:09 +0900

    error fix

--- Blackboard Alarm/popup.js
+++ Blackboard Alarm/popup.js
@@ -17,85 +17,102 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("블랙보드에 접속하여 강좌정보를 가져오세요");
             }
         }
-        var children = new Array();
-        const date_name = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "None"];
-        for (i in lecturelist) {
-            var temp = new Object();
-            temp["lecture"] = i
-            temp["first_time"] = lecturelist[i].first_date == 7 || lecturelist[i].first_date == undefined ? "-" : date_name[lecturelist[i].first_date] + "_" + lecturelist[i].first_time;
-            temp["second_time"] = lecturelist[i].second_date == 7 || lecturelist[i].second_date == undefined ? "-" : date_name[lecturelist[i].second_date] + "_" + lecturelist[i].second_time;
-            temp["third_time"] = lecturelist[i].third_date == 7 || lecturelist[i].third_date == undefined ? "-" : date_name[lecturelist[i].third_date] + "_" + lecturelist[i].third_time;
-            temp["collab"] = lecturelist[i].id;
-            temp["auto_join"] = lecturelist[i].use_collaborate == undefined ? '-'  : lecturelist[i].use_collaborate == true ? 'Yes' : 'No';
-            //console.log(lecturelist[i].third_date);
-            children.push(temp);
-        }
-        console.log(children);
+        var tablebody = document.getElementsByClassName("tablebody")[1].children[0].children[0];
+        var date_list = ["월", "화", "수", "목", "금"]
+        for (key in lecturelist) {
+            for (i in date_list) {
+                if (lecturelist[key]["time"].includes(date_list[i]) && i != undefined) {
+                    var a = parseInt(i) + 1
+                    var lecturename = document.createElement("div");
+                    lecturename.textContent = key
+                    tablebody.children[a].appendChild(lecturename);
+                    var lecturetime = document.createElement("div");
+                    lecturetime.textContent = "(" + lecturelist[key]["time"] + ")";
+                    tablebody.children[a].appendChild(lecturetime)
+                    var new_line = document.createElement("br");
+                    tablebody.children[a].appendChild(new_line.cloneNode());
 
-        function addHeaders(table, keys) {
-            var row = table.insertRow();
-            for (var i = 0; i < keys.length; i++) {
-                var cell = row.insertCell();
-                cell.appendChild(document.createTextNode(keys[i]));
-            }
-        }
-
-        var table = document.createElement('table');
-        for (var i = 0; i < children.length; i++) {
-
-            var child = children[i];
-            if (i === 0) {
-                addHeaders(table, Object.keys(child));
-            }
-            var row = table.insertRow();
-            Object.keys(child).forEach(function(k) {
-                var cell = row.insertCell();
-                if (k == "collab") {
-                    var btn = document.createElement("button");
-                    btn.className = "Collab_btn";
-                    btn.textContent = "Link";
-                    var icon = document.createElement("i");
-                    icon.className = 'fa fa-play'
-                    btn.append(icon);
-                    btn.setAttribute("id", child[k]);
-                    cell.appendChild(btn);
-                } else {
-                    cell.appendChild(document.createTextNode(child[k]));
                 }
-            })
+            }
         }
+        // var children = new Array();
+        // const date_name = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "None"];
+        // for (i in lecturelist) {
+        //     var temp = new Object();
+        //     temp["lecture"] = i
+        //     temp["first_time"] = lecturelist[i].first_date == 7 || lecturelist[i].first_date == undefined ? "-" : date_name[lecturelist[i].first_date] + "_" + lecturelist[i].first_time;
+        //     temp["second_time"] = lecturelist[i].second_date == 7 || lecturelist[i].second_date == undefined ? "-" : date_name[lecturelist[i].second_date] + "_" + lecturelist[i].second_time;
+        //     temp["third_time"] = lecturelist[i].third_date == 7 || lecturelist[i].third_date == undefined ? "-" : date_name[lecturelist[i].third_date] + "_" + lecturelist[i].third_time;
+        //     temp["collab"] = lecturelist[i].id;
+        //     temp["auto_join"] = lecturelist[i].use_collaborate == undefined ? '-'  : lecturelist[i].use_collaborate == true ? 'Yes' : 'No';
+        //     //console.log(lecturelist[i].third_date);
+        //     children.push(temp);
+        // }
 
-        document.getElementById('container').appendChild(table);
-        var Collab_btn = document.getElementsByClassName("Collab_btn");
-        for (var i = 0; i < Collab_btn.length; i += 1) {
-            (function() {
-                var Collab_Url = `https://blackboard.unist.ac.kr/webapps/collab-ultra/tool/collabultra/lti/launch?course_id=${Collab_btn[i].getAttribute('id')}`;
-                Collab_btn[i].addEventListener("click", () => {
-                    chrome.tabs.query({ url: Collab_Url }, function(tabs) {
-                        if (tabs.length) {
-                            chrome.tabs.create(tabs[0].id, { active: true });
-                        } else {
-                            chrome.tabs.create({ url: Collab_Url });
-                        }
-                    });
-                });
-            }());
-        }
+        // function addHeaders(table, keys) {
+        //     var row = table.insertRow();
+        //     for (var i = 0; i < keys.length; i++) {
+        //         var cell = row.insertCell();
+        //         cell.appendChild(document.createTextNode(keys[i]));
+        //     }
+        // }
+
+        // var table = document.createElement('table');
+        // for (var i = 0; i < children.length; i++) {
+
+        //     var child = children[i];
+        //     if (i === 0) {
+        //         addHeaders(table, Object.keys(child));
+        //     }
+        //     var row = table.insertRow();
+        //     Object.keys(child).forEach(function(k) {
+        //         var cell = row.insertCell();
+        //         if (k == "collab") {
+        //             var btn = document.createElement("button");
+        //             btn.className = "Collab_btn";
+        //             btn.textContent = "Link";
+        //             var icon = document.createElement("i");
+        //             icon.className = 'fa fa-play'
+        //             btn.append(icon);
+        //             btn.setAttribute("id", child[k]);
+        //             cell.appendChild(btn);
+        //         } else {
+        //             cell.appendChild(document.createTextNode(child[k]));
+        //         }
+        //     })
+        // }
+
+        // document.getElementById('container').appendChild(table);
+        // var Collab_btn = document.getElementsByClassName("Collab_btn");
+        // for (var i = 0; i < Collab_btn.length; i += 1) {
+        //     (function() {
+        //         var Collab_Url = `https://blackboard.unist.ac.kr/webapps/collab-ultra/tool/collabultra/lti/launch?course_id=${Collab_btn[i].getAttribute('id')}`;
+        //         Collab_btn[i].addEventListener("click", () => {
+        //             chrome.tabs.query({ url: Collab_Url }, function(tabs) {
+        //                 if (tabs.length) {
+        //                     chrome.tabs.create(tabs[0].id, { active: true });
+        //                 } else {
+        //                     chrome.tabs.create({ url: Collab_Url });
+        //                 }
+        //             });
+        //         });
+        //     }());
+        // }
     })
 
 
-    var btn0 = document.querySelector("#btn_option_popup");
-    btn0.addEventListener("click", () => {
-        if (document.location.href.includes("blackboard.unist.ac.kr/") && document.location.href.includes("tab_tab_group_id")) {} else {
-            var optionsUrl = chrome.runtime.getURL('options.html');
+    // var btn0 = document.querySelector("#btn_option_popup");
+    // btn0.addEventListener("click", () => {
+    //     if (document.location.href.includes("blackboard.unist.ac.kr/") && document.location.href.includes("tab_tab_group_id")) {} else {
+    //         var optionsUrl = chrome.runtime.getURL('options.html');
 
-            chrome.tabs.query({ url: optionsUrl }, function(tabs) {
-                if (tabs.length) {
-                    chrome.tabs.create(tabs[0].id, { active: true });
-                } else {
-                    chrome.tabs.create({ url: optionsUrl });
-                }
-            });
-        }
-    });
+    //         chrome.tabs.query({ url: optionsUrl }, function(tabs) {
+    //             if (tabs.length) {
+    //                 chrome.tabs.create(tabs[0].id, { active: true });
+    //             } else {
+    //                 chrome.tabs.create({ url: optionsUrl });
+    //             }
+    //         });
+    //     }
+    // });
 });

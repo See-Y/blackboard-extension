2867d96d4927e2db4a0f8aa2d0ba21d030a9a93f
SeeWhy <seewhy@unist.ac.kr>
2022-12-23 20:26:03 +0900

    little change

--- Scripts/Downloader/downloader.js
+++ Scripts/Downloader/downloader.js
@@ -1,5 +1,29 @@
-const bbPrefixUrl = "https://blackboard.unist.ac.kr/"
 var lastRowDiv = document.getElementsByClassName("lastRowDiv")[0];
+
+function downloads(doc){
+    const bbPrefixUrl = "https://blackboard.unist.ac.kr/";
+    var content = doc.getElementById("content_listContainer");
+    if (!isNaN(content)) return;
+
+    var aTags = content.getElementsByTagName("a");
+    for (var a of aTags) {
+        var link = a.getAttribute("href");
+        if(link == "#contextMenu") continue;
+        var tag = a.getAttribute("onclick");
+        var target = a.getAttribute("target");
+        if(isNaN(tag) || isNaN(target)){
+            var fileUrl = bbPrefixUrl+link;
+            chrome.runtime.sendMessage({sender: "downloader", url: fileUrl});
+            console.log(fileUrl);
+        }
+        else{
+            folder = window.open(bbPrefixUrl+link);
+            downloads(folder.document);
+            folder.close();
+        }
+    }
+}
+
 
 var downloadBtn = HTMLAppender({
     parent: lastRowDiv,
@@ -9,12 +33,7 @@ var downloadBtn = HTMLAppender({
     innerText: "Download All",
     eventListener:  {
         click: (evt) => {
-            var content = document.getElementById("content_listContainer");
-            if (!isNaN(content)) return;
-            var aTags = content.getElementsByTagName("a");
-            for (var a of aTags) {
-                var fileUrl = bbPrefixUrl+a.getAttribute("href");
-                chrome.runtime.sendMessage({sender: "downloader", url: fileUrl});            }
+            downloads(document);
         }
     }
 });

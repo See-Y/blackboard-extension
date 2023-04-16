8f18bdc90f384d238ad533572514f6220f616aa1
123wwwa <123wwa@naver.com>
2023-04-17 02:37:40 +0900

    update Timetable

--- src/content-script/CancelLogout/CancelLogout-append.js
+++ src/content-script/CancelLogout/CancelLogout-append.js
@@ -5,13 +5,11 @@
         window.bbSessionActivityTimer.showDialog = function() {
             //this.checkTimeRemainingBeforeTimeout();
             this.keepBbSessionActive();
-            console.log("Canceled Logout");
             return proxied.apply(this, [].slice.call(arguments));
         }
         window.bb_dialogs.bb_confirm = function() { return; } // cancel LogOut Popup
         window.bbSessionActivityTimer.keepBbSessionActiveIfNotTimedOut = function() {
             this.keepBbSessionActive();
-            console.log("Canceled Logout");
         }
     }
 })();
--- src/content-script/timeTable/App.tsx
+++ src/content-script/timeTable/App.tsx
@@ -1,133 +1,145 @@
 /// <reference types="chrome" />
 /// <reference types="vite-plugin-svgr/client" />
 
-import logo from 'assets/logo.svg'
 import React, { useEffect, useRef, useState } from 'react'
 import './App.css'
-import { Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, styled } from '@mui/material';
 
+const HeadHeight = 20;
+const TableHeight = 35;
+const RenderTableDay = () => {
+  const [lectureList, setLectureList] = useState<any>(null);
+  const [isLectureListLoaded, setIsLectureListLoaded] = useState(false);
+  const [shapedLectureList, setShapedLectureList] = useState<any>(null);
+  const [logoURL, setLogoURL] = useState<string>("");
+  const loadTimeTable = () => {
+    setLogoURL(chrome.runtime.getURL("src/assets/HeXA_logo.png"));
+    window.chrome.storage.sync.get(['lectureInfo'], (res) => {
+      if (res.lectureInfo == undefined && res.lectureInfo == null) {
+        alert("블랙보드에 접속하여 강좌정보를 가져오세요!(현재 접속중일경우 새로고침(F5))");
+      }
+      else {
+        var resLecturelist = JSON.parse(res.lectureInfo);
+        setLectureList(resLecturelist);
+        if (!resLecturelist || (Object.keys(resLecturelist).length === 0 && Object.getPrototypeOf(resLecturelist) === Object.prototype)) {
+          alert("블랙보드에 접속하여 강좌정보를 가져오세요!(현재 접속중일경우 새로고침(F5))");
+        }
+        var colorlist = ["#eff9cc", "#dee8f6", "#ffe9e9", "#ffedda", "#dcf2e9", "#dceef2", "#fff8cc", "#ffe9e9"]
+        var l: any[] = [[], [], [], [], []];
+        let key: string;
+        var i = 0;
+        for (key in resLecturelist) {
+          let item: any = resLecturelist[key];
+          i += 1;
+          for (var c = 0; c < 3; c++) {
+            if (item["timeplace" + c]) {
+              let newItem: any = {};
+              newItem["name"] = item["name"];
+              newItem["professor"] = item["professor"];
+              newItem["time"] = item["time"];
+              newItem["link"] = item["link"];
+              newItem["color"] = colorlist[i];
+              newItem["timeplace"] = item["timeplace" + c];
+              l[item["timeplace" + c].day].push(newItem);
 
+            }
+          }
+        }
+        setShapedLectureList(l)
+        setIsLectureListLoaded(true);
+      }
+    })
+  }
 
-function App() {
+  const LectureDiv = (props: any) => {
+    const marginTop: number = (props.item["timeplace"].start - (9 * 12)) / 12 * (TableHeight + 2) + (HeadHeight + 3); // minus 9 hour to start from 9 
+    // add 2 to consider margin.
+    const height: number = (props.item["timeplace"].end - props.item["timeplace"].start) / 12 * (TableHeight + 2);
+    const place = props.item["timeplace"].place;
+    const link = props.item["link"];
+    return (
+      <div>
+        <div
+          id="lectureDiv"
+          style={{
+            backgroundColor: props.item["color"],
+            marginTop: marginTop + 'px',
+            height: height + 'px',
+          }}
+          onClick={() => {
+            window.open(link, "_blank");
+          }}>
+          <span>{props.item["name"]}</span>
+          <span id="lecturePlace">{place}</span>
+        </div>
+      </div>
+    )
+  }
+  useEffect(() => {
+    loadTimeTable();
+  }, [])
+  const dayList = ["월", "화", "수", "목", "금"];
+  return (
+    <>
+      <div style={{
+        display: "flex", flexDirection: "row"
+      }}>
+        {isLectureListLoaded && <>
+          <div>
+            <div id="lectureGrid"
+              style={{
+                width: "20px", height: HeadHeight,
+              }}>
+                <img src={logoURL} 
+                style={{
+                  width: "20px", height: "20px",
+                }}/>
+            </div>
+            {[...Array(12)].map((x, j) => {
+              return (<div id="lectureGrid"
+                style={{
+                  width: "20px", height: TableHeight,
+                }}>
+                <span>
+                  {j + 9}
+                </span>
+              </div>)
+            })}
+          </div>
+          {[...Array(5)].map((x, i) => {
+            return (<div>
+              {shapedLectureList[i].map((item: any) => {
+                return (<>
+                  <LectureDiv item={item}></LectureDiv>
+                </>)
+              })}
+              <div id="lectureGrid"
+                style={{
+                  width: "59px", height: HeadHeight,
+                }}>
+                {dayList[i]}
+              </div>
+              {[...Array(12)].map(() => {
+                return (<div id="lectureGrid"
+                  style={{
+                    width: "59px", height: TableHeight,
+                  }}>
+                </div>)
+              })}
+            </div>)
+          })}
+        </>}
+      </div>
+    </>
+  )
+}
+const TimeTable = () => {
   const parentRef: any = useRef(null);
   const isParentLoaded = useRef(parentRef.current !== null);
   const [parentTop, setParentTop] = useState<number>(0);
   const [parentLeft, setParentLeft] = useState<number>(0);
   const [parentHeight, setParentHeight] = useState<number>(0);
-  const style = { top: parentTop, position: 'absolute', width: "57px" } as React.CSSProperties;
-  const BorderedTD = styled(TableCell)`
-  width: 57px;
 
-  
-  padding: 0;
-  &.MuiTableCell-root {
-    border: 1px solid #000;
-    font-weight: bold;
-    width: 57px;
-    height: 35px;
-    
-  }
-`;
-  const BorderedTD2 = styled(TableCell)`
-  width: 15px;
-  padding: 0;
-  &.MuiTableCell-root {
-    border: 1px solid #000;
-    font-weight: bold;
-    width: 15px;
-    height: 35px;
-    
-    
-  }
-`;
-  const Grid = styled('div')(({ theme }) => ({
-    display: 'grid',
-    gridTemplateColumns: 'repeat(7, 1fr)',
-    height: '35px',
-    width: '59px',
-    borderTop: '1px solid #e3e3e3',
-    boxSizing: 'border-box',
-  }));
-  const Grid2 = styled('div')(({ theme }) => ({
-    display: 'grid',
-    gridTemplateColumns: 'repeat(7, 1fr)',
-    height: '35px',
-    width: '15px',
-    borderTop: '1px solid #e3e3e3',
-    boxSizing: 'border-box',
-  }));
 
-  const RenderTableDay = () => {
-    const [lectureList, setLectureList] = useState<any>(null);
-    const [isLectureListLoaded, setIsLectureListLoaded] = useState(false);
-    const [shapedLectureList, setShapedLectureList] = useState<any>(null);
-    useEffect(() => {
-      window.chrome.storage.sync.get(['lectureInfo'], (res) => {
-        if (res.lectureInfo == undefined && res.lectureInfo == null) {
-          alert("블랙보드에 접속하여 강좌정보를 가져오세요!(현재 접속중일경우 새로고침(F5))");
-        }
-        else {
-          var resLecturelist = JSON.parse(res.lectureInfo);
-          setLectureList(resLecturelist);
-          if (!resLecturelist || (Object.keys(resLecturelist).length === 0 && Object.getPrototypeOf(resLecturelist) === Object.prototype)) {
-            alert("블랙보드에 접속하여 강좌정보를 가져오세요!(현재 접속중일경우 새로고침(F5))");
-          }
-          var colorlist = ["#eff9cc", "#dee8f6", "#ffe9e9", "#ffedda", "#dcf2e9", "#dceef2", "#fff8cc", "#ffe9e9"]
-          var l: any[] = [[], [], [], [], []];
-          let key: string;
-          var i = 0;
-          for (key in resLecturelist) {
-            let item: any = resLecturelist[key];
-            i+=1;
-            for (var c = 0; c < 3; c++) {
-              if (item["timeplace" + c]) {
-                let newItem: any = {};
-                newItem["name"] = item["name"];
-                newItem["professor"] = item["professor"];
-                newItem["time"] = item["time"];
-                newItem["link"] = item["link"];
-                newItem["color"] = colorlist[i];
-                newItem["timeplace"] = item["timeplace" + c];
-                l[item["timeplace" + c].day].push(newItem);
-
-              }
-            }
-          }
-          setShapedLectureList(l)
-          setIsLectureListLoaded(true);
-        }
-      })
-    }, [])
-    return (
-      <>
-        {isLectureListLoaded && <>
-          {[...Array(5)].map((x, i) => {
-            return (<BorderedTD>
-              {shapedLectureList[i].map((item: any) => {
-                const LectureDiv = styled(Box)`
-                position: absolute;
-                margin-top: ${(item["timeplace"].start - 108) * 840/288 + 'px'}; 
-                height: ${(item["timeplace"].end - item["timeplace"].start) * 2.8 + 'px'};
-                width: 59px;
-                font-family: "맑은 고딕";
-                font-size: 10px;
-                font-weight: bold;
-                background-color: ${item["color"]};
-                `;
-                
-                return (<>
-                  <LectureDiv>{item.name}</LectureDiv>
-                </>)
-              })}
-              {[...Array(12)].map((x, i) =>
-                <Grid></Grid>
-              )}
-            </BorderedTD>)
-          })}
-        </>}
-      </>
-    )
-  }
   return (
     <div ref={(el: any) => {
       const parentTop: number = el?.getBoundingClientRect().top - 140; //35*4 = 140 
@@ -136,51 +148,18 @@ function App() {
       setParentTop(parentTop);
       setParentLeft(parentLeft);
       setParentHeight(parentHeight);
-      console.log(parentTop, parentLeft, parentHeight);
-    }}>
-      <TableContainer sx={{ height: 440 }}>
-        <TableRow sx={{ height: '35px' }}>
+    }}
+      id="parent"
+      style={{
+        height: "440px",
+        width: "100%",
 
-          <BorderedTD2>
-            {[...Array(12)].map((x, i) =>
-              <Grid2>{i + 9}</Grid2>
-            )}
-          </BorderedTD2>
-          <RenderTableDay />
-        </TableRow>
-        {/* <RenderLectureList /> */}
-      </TableContainer>
-      {/* <RenderLectureList /> */}
-      {/* {parentTop ? <RenderLectureList /> : null} */}
-
-
+      }}>
+      <RenderTableDay />
     </div>
 
   )
 }
-// const TimeTable = () =>{
-//   return(
-//     <>
-//       <div className="tablehead">
-//             <table className="tablehead">
-//                 <tbody>
-//                     <tr>
-//                         <th>
-//                             <div className="setting"></div>
-//                         </th>
-//                         <td>월</td>
-//                         <td>화</td>
-//                         <td>수</td>
-//                         <td>목</td>
-//                         <td>금</td>
-//                     </tr>
-//                 </tbody>
-//             </table>
-//         </div>
-//     </>
-//   )
-// }
 
-export default App
-
+export default TimeTable;
 
